1b15ebcea5fc0de5c51d824ac2f47417bebc2f5f
Glitch (glitch-hello-node) <none>
2022-11-20 13:50:13 +0000

    ⛽️🚠 Checkpoint ./pingCommand.js:50931497/530 ./code.js:50931497/202

--- pingCommand.js
+++ pingCommand.js
@@ -1,9 +1,10 @@
+const { EmbedBuilder } = require("discord.js");
+
 function pingCommand(client) {
   client.on("interactionCreate", async i => {
     if (!i.isCommand()) {
         return;
     }
-      //ping command
     if (i.commandName === 'ping') {
         const e = new EmbedBuilder()
           .setColor('E841C4')
