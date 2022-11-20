fde98ce2af9b1e0a7a35d4fb2cb37d73860dbcb4
Glitch (glitch-hello-node) <none>
2022-11-20 14:47:17 +0000

    🐟💺 Checkpoint ./code.js:50931497/2038

--- code.js
+++ code.js
@@ -2,6 +2,8 @@ const { Discord , Client, GatewayIntentBits , EmbedBuilder , userMention , Messa
 
 const { poll } = require('discord.js-poll');
 
+const on_message = require('./Commands/poll.js')
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -92,43 +94,6 @@ client.on("interactionCreate", async i => {
     }
 });
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("poll")
-        .setDescription("Create a poll and send it to a certain channel")
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addStringOption(option =>
-            option.setName("description")
-                .setDescription("Describe the poll.")
-                .setRequired(true)
-        )
-        .addChannelOption(option =>
-            option.setName("channel")
-                .setDescription("Where do you want to send the poll to?")
-                .setRequired(true)
-                .addChannelTypes(ChannelType.GuildText)
-        ),
-    async execute(interaction) {
-        const { options } = interaction;
-
-        const channel = options.getChannel("channel");
-        const description = options.getString("description");
-
-        const embed = new EmbedBuilder()
-            .setColor("Gold")
-            .setDescription(description)
-            .setTimestamp();
-
-        try {
-            const m = await channel.send({ embeds: [embed] });
-            await m.react("✅");
-            await m.react("❌");
-            await interaction.reply({ content: "Poll was succesfully sent to the channel.", ephemeral: true });
-        } catch (err) {
-            console.log(err);
-        }
-    }
-}
     /*module.exports = {
         name: 'poll',
         description: 'Create a poll',
