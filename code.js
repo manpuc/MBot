de7f4b86f113b49f1d106d80e740a96da817c74b
Glitch (glitch-hello-node) <none>
2022-11-20 14:35:59 +0000

    🏰⏱ Checkpoint ./Commands/poll.js:50931497/1421 ./code.js:50931497/3999

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const { Discord , Client, GatewayIntentBits , EmbedBuilder , userMention , MessageEmbed, Message , ColorResolvable } = require("discord.js");
+const { Discord , Client, GatewayIntentBits , EmbedBuilder , userMention , MessageEmbed, Message , ColorResolvable , SlashCommandBuilder , PermissionFlagsBits , ChannelType } = require("discord.js");
 
 const { poll } = require('discord.js-poll');
 
@@ -91,6 +91,44 @@ client.on("interactionCreate", async i => {
 	      }], ephemeral: true});
     }
 });
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("poll")
+        .setDescription("Create a poll and send it to a certain channel")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addStringOption(option =>
+            option.setName("description")
+                .setDescription("Describe the poll.")
+                .setRequired(true)
+        )
+        .addChannelOption(option =>
+            option.setName("channel")
+                .setDescription("Where do you want to send the poll to?")
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildText)
+        ),
+    async execute(interaction) {
+        const { options } = interaction;
+
+        const channel = options.getChannel("channel");
+        const description = options.getString("description");
+
+        const embed = new EmbedBuilder()
+            .setColor("Gold")
+            .setDescription(description)
+            .setTimestamp();
+
+        try {
+            const m = await channel.send({ embeds: [embed] });
+            await m.react("✅");
+            await m.react("❌");
+            await interaction.reply({ content: "Poll was succesfully sent to the channel.", ephemeral: true });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
     /*module.exports = {
         name: 'poll',
         description: 'Create a poll',
