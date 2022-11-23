bcf5c630e5e3abc132a7439ce81e197159bf6084
Glitch (glitch-hello-node) <none>
2022-11-23 22:48:30 +0000

    💂🚙 Checkpoint ./code.js:50931497/34

--- code.js
+++ code.js
@@ -21,7 +21,7 @@ client.on('ready', async() => {
 client.on('messageCreate', message => {
     if (message.author.bot)return;
     if (message.content == 'hi') {
-        message.channel.send('<@971353847408767007>はspamをやめろ！');
+        message.channel.send('hi!');
     }
 });
 client.on('messageCreate', message => {
