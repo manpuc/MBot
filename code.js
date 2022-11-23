570f740705a3e60b25cb8f28119010a93ab5221c
Glitch (glitch-hello-node) <none>
2022-11-23 19:18:24 +0000

    💍📮 Checkpoint ./code.js:50931497/223

--- code.js
+++ code.js
@@ -98,14 +98,13 @@ client.on("interactionCreate", async i => {
 	      }], ephemeral: true});
     }
 });
-
-    /*module.exports = {
+    module.exports = {
         name: 'poll',
         description: 'Create a poll',
         usage: 'Title + Option 1 + Option 2 + Option 3 + etc',
         execute(client, message, args) {
           poll(message, args, '+', '#00D1CD');
         },
-    };*/
+    };
 //process.exit()
 client.login(process.env.DISCORD_BOT_TOKEN);
