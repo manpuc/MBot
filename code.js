e11ce3368349fe7ab55553f4ea151fe7bfc99761
Glitch (glitch-hello-node) <none>
2022-11-24 03:09:30 +0000

    🚤🌸 Checkpoint ./code.js:50931497/4

--- code.js
+++ code.js
@@ -119,13 +119,13 @@ client.on("interactionCreate", async i => {
       await i.reply();
     }
 });
-    module.exports = {
+    /*module.exports = {
         name: 'poll',
         description: 'Create a poll',
         usage: 'Title + Option 1 + Option 2 + Option 3 + etc',
         execute(client, message, args) {
           poll(message, args, '+', '#00D1CD');
         },
-    };
+    };*/
 //process.exit()
 client.login(process.env.DISCORD_BOT_TOKEN);
