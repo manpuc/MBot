b7e805a83a73423415444154eef5140730d33fe2
Glitch (glitch-hello-node) <none>
2022-12-03 04:58:29 +0000

    🍚🌼 Checkpoint ./Commands/poll.js:44504741/2889

--- Commands/poll.js
+++ Commands/poll.js
@@ -1,47 +1,46 @@
-const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, ChannelType } = require("discord.js");
+const {
+  ContextMenuCommandBuilder,
+  ApplicationCommandType,
+  EmbedBuilder
+} = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("poll")
-        .setDescription("Create a poll and send it to a certain channel")
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addStringOption(option =>
-            option.setName("description")
-                .setDescription("Describe the poll.")
-                .setRequired(true)
-        )
-        .addChannelOption(option =>
-            option.setName("channel")
-                .setDescription("Where do you want to send the poll to?")
-                .setRequired(true)
-                .addChannelTypes(ChannelType.GuildText)
-        ),
-    async execute(interaction) {
-        const { options } = interaction;
+  data: new ContextMenuCommandBuilder()
+    .setName('IDを取得')　
+    .setType(ApplicationCommandType.Message),
+  async execute(i) {
+    const e = new EmbedBuilder()
+      .setColor(process.env.C)
+      .setTitle(`${("` " +　i.targetMessage.cleanContent.substr( 0, 1000 )+ "`")}`)
+      .setAuthor({
+        name: i.targetMessage.author.tag,
+        iconURL: i.targetMessage.author.displayAvatarURL({
+          format: 'png'
+        })
+      })
+      .addFields({
+        name: 'メッセージID',
+        value: "```javascript\n" + i.targetMessage.id + "\n```",
+        inline: false
+      }, {
+        name: 'サーバーID',
+        value: "```javascript\n" + i.targetMessage.guild.id + "\n```",
+        inline: false
+      }, {
+        name: 'チャンネルID',
+        value: "```javascript\n" + i.targetMessage.channel.id + "\n```",
+        inline: false
+      }, {
+        name: 'ユーザーID',
+        value: "```javascript\n" + i.targetMessage.author.id + "\n```",
+        inline: false
+      }, )
 
-        const channel = options.getChannel("channel");
-        const description = options.getString("description");
 
-        const embed = new EmbedBuilder()
-            .setColor("Gold")
-            .setDescription(description)
-            .setTimestamp();
-
-        try {
-            const m = await channel.send({ embeds: [embed] });
-            await m.react("✅");
-            await m.react("❌");
-            await interaction.reply({ content: "Poll was succesfully sent to the channel.", ephemeral: true });
-        } catch (err) {
-            console.log(err);
-        }
-    }
+    await i.reply({
+      embeds: [e],
+      ephemeral: true
+    });
+  },
 }
-
-    /*if (i.author.bot || i.channel.type === 'dm') return;
-       const messageArray = i.content.split(' ');
-       const cmd = messageArray[0];
-       const args = messageArray.slice(1);
-    if (i.commandName === 'poll') {
-      
-    }*/
+//i.targetMessage.author.displayAvatarURL({ format: 'png' })
