b431792d4a01287db982015dcc545e55842ceb05
Glitch (spicy-sudsy-manuscript) <none>
2022-12-21 08:41:34 +0000

    😋⚓️ Checkpoint ./code.js:44504741/1063:50931497/23205

--- code.js
+++ code.js
@@ -21,11 +21,11 @@ client.on('ready', async() => {//完全不潔のMBot
 client.on('messageCreate', message => {
     if (message.author.bot)return;
     if (message.content == 'くぁｗせｄｒｆｔｇｙふじこｌｐ') {
-        message.channel.send(`${userMention(message.author.id)}とりまﾀﾋね。`);//ok　ごみ　将来の炎上要素wwwwwww
+        message.channel.send(`${userMention(message.author.id)}とりま落ち着け。`);//
       const userId = message.author.id; 
       client.users.send( userId , `<@${userId}>さんこんにちは　MBotはサポートが終了しました\nセキュリティーに問題があるので今すぐMBotを削除してください`)//943457413367996466
     }
-});//w
+});
 
 module.exports = {
 	name: 'poll',
@@ -96,4 +96,4 @@ client.on("interactionCreate", async i => {
 });
 
 //process.exit()
-client.login(process.env.DISCORD_BOT_TOKEN)//w
+client.login(process.env.DISCORD_BOT_TOKEN);
