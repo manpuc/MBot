0387ed62f5b6eaac08fb82bde2f224863e4239d1
Glitch (spicy-sudsy-manuscript) <none>
2023-01-18 07:41:27 +0000

    👼🌮 Checkpoint ./poll.js:50931497/417 ./code.js:50931497/1150

--- code.js
+++ code.js
@@ -94,7 +94,40 @@ client.on("interactionCreate", async (i) => {
       await msg.react("🟢");
       await msg.react("🔵");
     });*/
-  }
+  }:
+  //poll command 2
+      data: new SlashCommandBuilder()
+        .setName("poll")
+        .setDescription("簡易的な投票('⭕'or'❌')")
+        .setDefaultMemberPermissions (PermissionFlagsBits.Administrator)
+        .addStringOption (option =>
+            option.setName("description")
+                .setDescription("なんの")
+                .setRequired(true)
+        )
+        .addChannelOption (option =>
+            option.setName("channel") 
+                .setDescription("どこで")
+                .setRequired(true)
+        ),
+    async execute(interaction){
+        const { options } = interaction;
+      
+        const channel = options.getChannel("channel");
+        const description = options.getString("description");
+        const embed = new EmbedBuilder()
+            .setColor("Gold")
+            .setDescription(description)
+            .setTimestamp();
+        try {
+            const m = await channel.send({ embeds: [embed] });
+            await m.react("⭕");
+            await m.react("❌");
+            await interaction.reply({ content: "問題なく動作", ephemeral: true });
+        } catch (err) {
+            console.log(err);
+        }
+    }
   //ping command
   if (i.commandName === "ping") {
     const e = new EmbedBuilder()
--- poll.js
+++ poll.js
@@ -4,34 +4,34 @@ module.exports = {
 
     data: new SlashCommandBuilder()
         .setName("poll")
-        .setDescription("Create a poll and send it to a certain channel")
+        .setDescription("簡易的な投票('⭕'or'❌')")
         .setDefaultMemberPermissions (PermissionFlagsBits.Administrator)
         .addStringOption (option =>
             option.setName("description")
-                .setDescription("Describe the poll.")
+                .setDescription("なんの")
                 .setRequired(true)
         )
         .addChannelOption (option =>
             option.setName("channel") 
-                .setDescription("Where do you want to send the poll to?")
+                .setDescription("どこで")
                 .setRequired(true)
         ),
     async execute(interaction){
         const { options } = interaction;
       
-      
+        const channel = options.getChannel("channel");
         const description = options.getString("description");
         const embed = new EmbedBuilder()
-          .setColor("Gold")
-          .setDescription(description)
-          .setTimestamp();
-          try {
+            .setColor("Gold")
+            .setDescription(description)
+            .setTimestamp();
+        try {
             const m = await channel.send({ embeds: [embed] });
-            await m.react("a");
-            await m.react("b");
-            await interaction.reply({ content: "Poll was succesfully sent to the channel.", ephemeral: true });
-          } catch (err) {
-              console.log(err);
-          }
+            await m.react("⭕");
+            await m.react("❌");
+            await interaction.reply({ content: "問題なく動作", ephemeral: true });
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
